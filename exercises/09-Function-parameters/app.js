2feb561236637277db6f29ee1af90c4eb844d8f9
María Celeste Alpízar Solano <maria.alpizar.72@gmail.com>
2021-03-03 04:34:33 +0000

    Finish

--- exercises/09-Function-parameters/app.js
+++ exercises/09-Function-parameters/app.js
@@ -1,6 +1,14 @@
 // Your code goes here:
+function renderPerson(name, birthday, eyescolor, age, gender){
+    return name + " is a " + age + " years old " + gender + " born in " + birthday + " with " + eyescolor + " eyes ";
+}
 
-
+// var name = "Bob";
+// var birthday = "05/22/1983";
+// var eyescolor = "green";
+// var age = "23";
+// var gender = "male";
+// var ren = name.concat(birthday, eyescolor, age, gender);
 
   ///*** Do not edit below this line ****/
   console.log(renderPerson('Bob', '05/22/1983', 'green', 23, 'male'));
